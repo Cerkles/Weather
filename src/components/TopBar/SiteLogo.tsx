afb5c846466066bf16e4308ac0d5a5cbf1238cb9
Corey <coreydblevins@gmail.com>
2024-02-15 13:29:16 -0500

    added react-icons

--- src/components/TopBar/SiteLogo.tsx
+++ src/components/TopBar/SiteLogo.tsx
@@ -1,14 +1,19 @@
+import { RiCloudFill } from "react-icons/ri";
 
 export function SiteLogo() {
 
     return (
         <div className={styles.root}>
+            <div className={styles.cloud}>
+            <RiCloudFill />
+            </div>
             <p className={styles.text}>{"How's the Weather?"}</p>
         </div>
     )
 }
 
 const styles = {
-    root: "flex text-white",
-    text: "text-lg font-medium"
+    root: "flex items-center text-white",
+    cloud: "scale-150",
+    text: "px-2 text-xl font-medium"
 }
--- src/components/TopBar/Tabs.tsx
+++ src/components/TopBar/Tabs.tsx
@@ -1,23 +1,31 @@
 "use client"
 import { useState } from "react"
+import { FaCloud } from "react-icons/fa";
+import { MdDashboard } from "react-icons/md";
+
+
 
 export function Tabs() {
     const [selectedTab, setSelectedTab] = useState(0)
-    const tabs = ["Overview", "Dashboard"]
+    const tabs = [
+        { icon: <FaCloud />, title: "Overview" },
+        { icon: <MdDashboard />, title: "Dashboard" }
+    ]
 
     return (
         <div className={styles.root}>
-            {tabs.map((title, index) => {
+            {tabs.map((tab, index) => {
                 const underline = selectedTab === index ?
                     "border-b-2 border-white"
                     : "border-b-2 border-transparent"
 
                 return (
                     <button key={`button_${index}`}
-                        className={`transform ease-in-out duration-500 ${underline}`}
+                        className={`${styles.tab} ${underline}`}
                         onClick={() => setSelectedTab(index)}
                     >
-                        {title}
+                        <div>{tab.icon}</div>
+                        <p>{tab.title}</p>
                     </button>
                 )
             })}
@@ -26,5 +34,6 @@ export function Tabs() {
 }
 
 const styles = {
-    root: "flex gap-4 text-white text-lg font-medium"
+    root: "flex gap-4 text-white text-lg font-medium",
+    tab: "flex items-center gap-2 transform ease-in-out duration-500"
 }
